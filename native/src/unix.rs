b5ea7a3ef6cde94f683777b245a8a1eaf5244c91
Charles Lowell <cowboyd@frontside.com>
2020-11-05 17:08:45 -0600

    remove unix code altogether

--- native/src/unix.rs
+++
@@ -1,5 +0,0 @@
-use neon::prelude::*;
-
-pub fn ctrlc(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-    cx.throw_type_error("tried to invoke windows-specific ctrlc on a non-windows platform.")
-}
