35a20dc3c207bda26f5c4df2f274557e8f3c8de5
Charles Lowell <cowboyd@frontside.com>
2020-11-06 08:32:22 -0600

    Add a test to make sure that shutdown output is received

--- test/windows.test.ts
+++ test/windows.test.ts
@@ -1,4 +1,5 @@
 import { ChildProcess, spawn } from 'child_process';
+import expect from 'expect';
 
 import { ctrlc } from '../lib/index';
 
@@ -13,7 +14,9 @@ if (process.platform === "win32") {
     describe('on a running process', () => {
       let child: ChildProcess;
       let promise: Promise<unknown>;
+      let stdout: string;
       beforeEach(async () => {
+        stdout = '';
         promise = new Promise((resolve, reject)=> {
           child = spawn("node", ["./test/fixtures/daemon.js"], {
             stdio: 'pipe',
@@ -21,6 +24,7 @@ if (process.platform === "win32") {
           });
           child.on('error', reject);
           child.on('exit', resolve);
+          child.stdout?.on('data', chunk => stdout += chunk);
         });
         await new Promise(resolve => setTimeout(resolve, 100));
 
@@ -28,6 +32,7 @@ if (process.platform === "win32") {
       });
       it('causes that process to exit', async () => {
         await promise;
+        expect(stdout).toContain("exiting...")
       });
     });
   });
